02de5ed052dca445afaae4c7fe95a8999e5d3214
dionesiusap <dionesiusap@gmail.com>
2017-09-05 20:36:12 +0700

    Update graph.py

--- graph.py
+++ graph.py
@@ -4,7 +4,7 @@ import networkx as nx
 import itertools
 
 G = nx.Graph()
-    
+
 #Menambah node
 L = ['a','b','c','d','e','f','g','h']
 G.add_nodes_from(L)
@@ -19,12 +19,9 @@ edges = list()
 for pair in pairs:
     edges.append(pair)
 
-G.add_edges_from(edges)
-
-#Memberi style pada graf
-
+for edge in edges:
+    G.add_edge(*edge)
 
 #Menampilkan gambar
-nx.draw_circular(G, with_labels = True)
+nx.draw_circular(G, with_labels = True, edge_color='b')
 matplotlib.pyplot.show()
-
